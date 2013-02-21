ebbd58fc8cd24d7e45bfb837bb0178c97c6d467b
Ben Swinburne <ben.swinburne@gmail.com>
2013-02-21 10:57:46 +0000

    Added placeholder response class

---
+++ src/Omnipay/SecureTrading/Response.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Omnipay package.
+ *
+ * (c) Ben Swinburne <ben.swinburne@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Omnipay\Billing\SecureTrading;
+
+use Omnipay\AbstractResponse;
+use Omnipay\Exception;
+use Omnipay\Exception\InvalidResponseException;
+
+/**
+ * SecureTrading Response
+ */
+class Response extends AbstractResponse
+{
+}
